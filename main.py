a5b4c427b3e70bc5d88fe30e0abcb9a0ce56afc8
Krystian Opryszek <kopryszek@gmail.com>
2021-11-09 18:29:56 +0000

    Created main python file

---
+++ main.py
@@ -0,0 +1,13 @@
+# Clock In Managment System Using Face Recognition
+
+from tkinter import * 
+import tkinter as tk
+
+# User Interface (Menu) 
+
+window = tk.Tk()
+window.geometry("1028x520")
+window.resizable(True,False)
+window.title("Clocking System")   
+
+window.mainloop()
