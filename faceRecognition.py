2dcbd060b636731a96ee3fc4ea12d6bfe30e2309
Krystian Opryszek <kopryszek@gmail.com>
2021-11-17 17:41:32 +0000

    Accessing the images folder

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,4 +1,22 @@
-# Script for recognizing face after it's saved in the images folder
+import cv2
+import numpy as np
+import face_recognition
+from datetime import datetime
+import os
 
-# Testing
-print("faceRecognition script working!")
+def runRecognition():
+    path = 'images'
+    # list of images
+    images = []
+    # names of images
+    classNames = []
+    myList = os.listdir(path)
+    # use the names of images and import them 
+    for element in myList:
+        currentImg = cv2.imread(f'{path}/{element}')
+        # append currentImg
+        images.append(currentImg)
+        # append classNames
+        classNames.append(os.path.splitext(element)[0])
+    print("List of all images found:")
+    print(classNames)
