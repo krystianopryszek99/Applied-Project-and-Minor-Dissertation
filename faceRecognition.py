d0ef64040041200185707611097f6132a1f6d94d
Krystian Opryszek <kopryszek@gmail.com>
2021-11-18 08:57:39 +0000

    Added function for computing all the encodings

--- faceRecognition.py
+++ faceRecognition.py
@@ -20,3 +20,19 @@ def runRecognition():
         classNames.append(os.path.splitext(element)[0])
     print("List of all images found:")
     print(classNames)
+
+    # function that will compute all the encodings
+    def getEncodings(images):
+        # list with all encodings
+        encodeList = []
+        # looping through all images
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # finding the encodings
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+
+    # This function will get all faces that it knows 
+    getEncodeList = getEncodings(images)
+    print('Found Encodings')
