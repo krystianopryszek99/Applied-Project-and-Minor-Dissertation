aad03f64c526d6229c243e1b3bde46abecc5e5c5
Krystian Opryszek <kopryszek@gmail.com>
2021-11-20 12:15:01 +0000

    Displaying the name of the best match through webcam

--- faceRecognition.py
+++ faceRecognition.py
@@ -46,7 +46,7 @@ def runRecognition():
         resizedImg = cv2.resize(img,(0,0),None,0.25,0.25)
         # convert into rgb
         resizedImg = cv2.cvtColor(resizedImg, cv2.COLOR_BGR2RGB)
-
+        
         # find location of the faces
         facesCurrentFrame = face_recognition.face_locations(resizedImg)
         encodesCurrentFrame = face_recognition.face_encodings(resizedImg,facesCurrentFrame)
@@ -60,9 +60,16 @@ def runRecognition():
             faceDistance = face_recognition.face_distance(getEncodeList,encodeFace)
             print(faceDistance)
             matchIndex = np.argmin(faceDistance)
-            # show name of the best match
-            name = classNames[matchIndex].upper()
-            print(name)
+            
+            if matches[matchIndex]:
+                # show name of the best match
+                name = classNames[matchIndex].upper()
+                print(name)
+                y1,x2,y2,x1 = faceLocation
+                y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),2)
+                cv2.rectangle(img,(x1,y2-35),(x2,y2),(255,0,0),cv2.FILLED)
+                cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
         
         # Display the resulting frame
         cv2.imshow('Webcam',img)
