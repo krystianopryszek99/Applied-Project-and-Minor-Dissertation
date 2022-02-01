b58034119b34d5bd69e84abf6eaa50d93b7d396f
Krystian Opryszek <kopryszek@gmail.com>
2022-02-01 16:33:02 +0000

    Server parameters for email notification system

--- emailNotification.py
+++ emailNotification.py
@@ -15,3 +15,10 @@ def email_notification(to, subject, body):
     user_name = "email goes here"
     message['from'] = user_name
     password = "password goes here"
+
+    # Server parameters
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user_name, password)
+    server.send_message(message)
+    server.quit()
